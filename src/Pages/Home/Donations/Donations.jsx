5ec333e787be074fe72b2c8aff98d7efc0e16fe0
sajid9962 <clashking1545@gmail.com>
2022-11-10 22:24:22 +0600

    search implemented in server side

--- src/Pages/Home/Donations/Donations.jsx
+++ src/Pages/Home/Donations/Donations.jsx
@@ -43,9 +43,10 @@ const Donations = () => {
 			<div className="flex space-x-5 w-2/4 justify-center mx-auto my-5 space-y-5 md:space-y-0 flex-col md:flex-row">
 				{[...Array(pages).keys()].map((number) => (
 					<button
+						title={`page ${number + 1}`}
 						key={number}
-						className={` inline-flex items-center justify-center w-8 h-8 text-sm border rounded shadow-md dark:bg-gray-900 dark:border-gray-800 text-white font-semibold ${
-							page === number ? 'bg-yellow-700 font-bold text-3xl' : ''
+						className={` inline-flex items-center justify-center w-8 h-8 text-sm border rounded shadow-md  dark:border-gray-800  font-semibold ${
+							page === number ? 'text-red-600 dark:bg-gray-900 font-extrabold text-3xl' : ''
 						}`}
 						onClick={() => setPage(number)}>
 						{number + 1}
